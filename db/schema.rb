e241b515f8fdb75b3b352e60d11a8386565cd578
danodic <danilo.guimaraes@outlook.com.br>
2015-06-05 18:09:36 -0300

    Scaffold - Locations
    Geração da tabela Locations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602234452) do
+ActiveRecord::Schema.define(version: 20150605210147) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "sectors", force: :cascade do |t|
     t.string   "name"
