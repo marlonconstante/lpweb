a960a6d81805365625246161d72680787ed01700
danodic <danilo.guimaraes@outlook.com.br>
2015-06-11 00:36:38 -0300

    Index
    Adiciona a página de index com links para todas as outras páginas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  #get 'welcome/index'
+  
+  # You can have the root of your site routed with "root"
+  root 'welcome#index'
+
   resources :equipment
   resources :employees
   resources :equipment_types
@@ -6,9 +11,6 @@ Rails.application.routes.draw do
   resources :sectors
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
