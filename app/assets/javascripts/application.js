5910236a6ae28029d7fef18a054965ed208229fd
Marlon Santos Constante <marlon.constante@gmail.com>
2015-06-16 20:36:46 -0300

    Importando script e css do plugin: jquery.dataTables.bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,12 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+    $('.table').DataTable({});
+});
