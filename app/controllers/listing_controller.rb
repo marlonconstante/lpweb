1bbc121616489f0e8e4935cd78fe6f5d7ed95aa8
Danilo Guimaraes <danilo.guimaraes@outlook.com.br>
2015-06-16 20:23:48 -0300

    Listing - Updates on fields
    Updates nos campos da tabela listings

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,7 +1,8 @@
 class ListingController < ApplicationController
 	
 	def index
-		@listing = Equipment.joins(:employee).select("employees.*, equipment.*")
+		@listing_associated = Equipment.joins(:equipment_type).joins(:employee).select("employees.*, equipment.*")
+		@listing_available = Equipment.joins(:equipment_type).select("equipment.*").where("employee_id is null")
 	end
 	
 end
