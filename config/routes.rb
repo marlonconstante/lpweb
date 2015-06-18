2657274cafabcb96b66da33fcefe885b679b080b
danodic <danilo.guimaraes@outlook.com.br>
2015-06-18 03:48:48 -0300

    Layout - Versão inicial
    Versão inicial do layout e mudança para index redirecionar para
    listagem.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   #get 'welcome/index'
   
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'listing#index'
 
   resources :equipment
   resources :employees
