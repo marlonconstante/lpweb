e86426e9aadee2f04df1cdb4940f1a4718380d68
Marlon Santos Constante <marlon.constante@gmail.com>
2015-06-30 08:07:11 -0300

    Atualizando esquema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629225824) do
+ActiveRecord::Schema.define(version: 20150629225124) do
 
   create_table "employees", force: :cascade do |t|
     t.string   "name"
@@ -52,13 +52,6 @@ ActiveRecord::Schema.define(version: 20150629225824) do
 
   create_table "sectors", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "user_sessions", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
